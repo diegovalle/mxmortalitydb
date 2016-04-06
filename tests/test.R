720c544ec3a8005dae397fdb7ea63dfed33fb811
Diego Valle-Jones <diegovalle@gmail.com>
2016-04-06 17:58:33 -0500

    tests

--- tests/test.R
+++ tests/test.R
@@ -1,2 +1,33 @@
 library(testthat)
-test_dir("testthat")
+library(mxmortalitydb)
+library(plyr)
+
+test_check("testthat")
+
+# Encoding(aggressor.relation.code$relationship) <- "UTF-8"
+# save(aggressor.relation.code, file = "data/aggressor.relation.code.RData",
+#      compress = "xz")
+# 
+# Encoding(big.municipios$name) <- "UTF-8"
+# save(big.municipios, file = "data/big.municipios.RData",
+#      compress = "xz")
+# 
+# Encoding(geo.codes$name) <- "UTF-8"
+# save(geo.codes, file = "data/geo.codes.RData",
+#      compress = "xz")
+# 
+# Encoding(icd.103$icd_103_name) <- "UTF-8"
+# save(icd.103, file = "data/icd.103.RData",
+#      compress = "xz")
+# 
+# Encoding(metro.areas$metro_area) <- "UTF-8"
+# save(metro.areas, file = "data/metro.areas.RData",
+#      compress = "xz")
+# 
+# Encoding(mex.list$DESCRIP) <- "UTF-8"
+# save(mex.list, file = "data/mex.list.RData",
+#      compress = "xz")
+# 
+# Encoding(mex.list.group$mex_list_group_name) <- "UTF-8"
+# save(mex.list.group, file = "data/mex.list.group.RData",
+#      compress = "xz")
--- tests/testthat/test_match_totals.R
+++ tests/testthat/test_match_totals.R
@@ -1,26 +1,17 @@
-## Testing Script
-
-expect_that(1, equals(1))
-test_that("maximum age", {
-    expect_that(max(injury.intent$age_years, na.rm = TRUE), equals(120))
-})
-
 ## compare with the numbers
 ## available at 
 ## http://www.inegi.org.mx/est/contenidos/espanol/proyectos/continuas/vitales/bd/mortalidad/MortalidadGeneral.asp?s=est&c=11144
 
-suicides <- c(4117, 4315, 4277, 4395, 
-              4681, 5190, 5012, 5718, 5550)
-na <- c(2957, 2932, 2793, 2376, 
-        2567, 2920, 3594, 5630, 4375)
-homicides <- c(9330, 9926, 10454,
-               8868, 14007, 19804,
-               25757, 27213, 25967)
-accidents <- c(34880, 35865, 36282,
-               39343, 38880, 39461,
-               38120, 36694, 37729)
+
+test_that("maximum age", {
+  expect_that(max(injury.intent$age_years, na.rm = TRUE), equals(120))
+})
 
 test_that("number of registered homicides", {
+  homicides <- c(9330, 9926, 10454,
+                 8868, 14007, 19804,
+                 25757, 27213, 25967,  23063, 20013)
+  
   expect_that(ddply(subset(injury.intent, intent =="Homicide"),
                     .(year_reg), nrow)$V1,
               equals(
@@ -28,6 +19,9 @@ test_that("number of registered homicides", {
 
 
 test_that("number of registered accidents", {
+  accidents <- c(34880, 35865, 36282,
+                 39343, 38880, 39461,
+                 38120, 36694, 37729,36295, 35782)
   expect_that(ddply(subset(injury.intent, intent =="Accident"),
                     .(year_reg), nrow)$V1,
               equals(
@@ -35,12 +29,17 @@ test_that("number of registered accidents", {
 
 
 test_that("number of registered suicides", {
+  suicides <- c(4117, 4315, 4277, 4395, 
+                4681, 5190, 5012, 5718, 5550, 5909, 6337)
   expect_that(ddply(subset(injury.intent, intent =="Suicide"),
                     .(year_reg), nrow)$V1,
               equals(
                 suicides ))})
 
 test_that("number of registered deaths of unknown intent", {
+  
+  na <- c(2957, 2932, 2793, 2376, 
+          2567, 2920, 3594, 5630, 4375,4198, 4375)
   expect_that(ddply(subset(injury.intent, is.na(intent)),
                     .(year_reg), nrow)$V1,
               equals(
