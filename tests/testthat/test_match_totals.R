331ceddf1e77c0786445534b68fc61ac1368c306
diego <git@diegovalle.net>
2021-01-14 18:00:51 -0600

    2019 data

--- tests/testthat/test_match_totals.R
+++ tests/testthat/test_match_totals.R
@@ -12,7 +12,7 @@ test_that("number of registered homicides", {
   homicides <- c(9330, 9926, 10454,
                  8868, 14007, 19804,
                  25757, 27213, 25967,  
-                 23063, 20013, 20763, 24560, 32082, 36687)
+                 23063, 20013, 20763, 24560, 32082, 36687,36662)
   
   expect_that((injury.intent %>% 
                 filter(intent =="Homicide") %>%
@@ -27,7 +27,7 @@ test_that("number of registered accidents", {
   accidents <- c(34880, 35865, 36282,
                  39343, 38880, 39461,
                  38120, 36694, 37729, 
-                 36295, 35817, 37190, 37429, 36220,34591)
+                 36295, 35817, 37190, 37429, 36220,34591,33525)
   expect_that((injury.intent %>% 
                  filter(intent =="Accident") %>%
                  group_by(year_reg) %>% 
@@ -39,7 +39,7 @@ test_that("number of registered accidents", {
 test_that("number of registered suicides", {
   suicides <- c(4117, 4315, 4277, 4395, 
                 4681, 5190, 5012, 5718, 
-                5550, 5909, 6337, 6425, 6370, 6559, 6808)
+                5550, 5909, 6337, 6425, 6370, 6559, 6808,7225)
   expect_that((injury.intent %>% 
                  filter(intent =="Suicide") %>%
                  group_by(year_reg) %>% 
@@ -51,7 +51,7 @@ test_that("number of registered deaths of unknown intent", {
   # 2014 - 4,335
   na <- c(2957, 2932, 2793, 2376, 
           2567, 2920, 3594, 5630, 
-          4375, 4198, 4376, 4122, 4393, 5427, 5567)
+          4375, 4198, 4376, 4122, 4393, 5427, 5567,6399)
   expect_that((injury.intent %>% 
                  filter(is.na(intent)) %>%
                  group_by(year_reg) %>% 
@@ -65,7 +65,7 @@ test_that("number of registered deaths by Legal Intervention", {
   mi <- c(39, 72, 48, 47, 39,
           34, 37, 65, 115, 120,
           97, 77, 69,
-          112, 96)
+          112, 96, 71)
   expect_that((injury.intent %>% 
                  filter(intent == "Legal Intervention") %>%
                  group_by(year_reg) %>% 
